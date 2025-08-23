5366c61625d9d35fcdc9395d9f44352ef2a38bd4
miguelfocus <migueldfocus@gmail.com>
2025-08-23 23:33:26 +0200

    Fix settings 4

--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@ WRITEN_TOPICS_FILE_PATH = 'writen_topics.csv'
 
 if os.environ.get("GITHUB_ACTIONS"):
     WORKDIR = '/home/runner/work/AIHistorianBlogger/AIHistorianBlogger'
-    CONTENT_DIR_PATH = f'{WORKDIR}content'
-    WRITEN_TOPICS_FILE_PATH = f'{WORKDIR}writen_topics.csv'
+    CONTENT_DIR_PATH = f'{WORKDIR}/content'
+    WRITEN_TOPICS_FILE_PATH = f'{WORKDIR}/writen_topics.csv'
 else:
     from dotenv import load_dotenv
     load_dotenv()
