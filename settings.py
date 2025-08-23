cad818412fa91d6ff09efec86359e08b91eb0d5f
miguelfocus <migueldfocus@gmail.com>
2025-08-23 23:20:38 +0200

    Fix settings

--- settings.py
+++ settings.py
@@ -1,25 +1,25 @@
 import os
-from dotenv import load_dotenv
-
-INPUT_CONFIG = {
-    'title_words': (10, 50),
-    'body_words': (500, 1000),
-    'conclusion_words': (100, 300),
-}
 
 CRAZYNESS_LEVEL = 0
 
 CONTENT_DIR_PATH = 'content'
 WRITEN_TOPICS_FILE_PATH = 'writen_topics.csv'
 
-if os.environ.get("GITHUB_ACTIONS") != "true":
-    print('HELLO')
-    load_dotenv()
+if os.environ.get("GITHUB_ACTIONS"):
     REPOSITORY_NAME = 'AIHistorianBlogger'
     WORKDIR = f'/home/runner/work/{REPOSITORY_NAME}/{REPOSITORY_NAME}'
     CONTENT_DIR_PATH = f'{WORKDIR}/content'
     WRITEN_TOPICS_FILE_PATH = f'{WORKDIR}/writen_topics.csv'
+else:
+    from dotenv import load_dotenv
+    load_dotenv()
 
 OPENAI_CONFIG = {
     'api_key': os.getenv('OPENAI_API_KEY')
+}
+
+INPUT_CONFIG = {
+    'title_words': (10, 50),
+    'body_words': (500, 1000),
+    'conclusion_words': (100, 300),
 }
