9b75b3dec5e6e7efdf5b12171cc9b6d5fe6f8ed2
miguelfocus <migueldfocus@gmail.com>
2025-08-23 23:18:13 +0200

    Fix getting env variables

--- settings.py
+++ settings.py
@@ -1,12 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-print(os.environ)
-
-OPENAI_CONFIG = {
-    'api_key': os.getenv('OPENAI_API_KEY')
-}
-
 INPUT_CONFIG = {
     'title_words': (10, 50),
     'body_words': (500, 1000),
@@ -18,10 +12,14 @@ CRAZYNESS_LEVEL = 0
 CONTENT_DIR_PATH = 'content'
 WRITEN_TOPICS_FILE_PATH = 'writen_topics.csv'
 
-if os.getenv("GITHUB_ACTIONS") != "true":
+if os.environ.get("GITHUB_ACTIONS") != "true":
     print('HELLO')
     load_dotenv()
     REPOSITORY_NAME = 'AIHistorianBlogger'
     WORKDIR = f'/home/runner/work/{REPOSITORY_NAME}/{REPOSITORY_NAME}'
     CONTENT_DIR_PATH = f'{WORKDIR}/content'
     WRITEN_TOPICS_FILE_PATH = f'{WORKDIR}/writen_topics.csv'
+
+OPENAI_CONFIG = {
+    'api_key': os.getenv('OPENAI_API_KEY')
+}
