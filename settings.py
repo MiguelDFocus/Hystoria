8289916e4499f345f8419795e55183d8fe80cb1f
miguelfocus <migueldfocus@gmail.com>
2025-09-06 21:33:14 +0200

    Fix workdir

--- settings.py
+++ settings.py
@@ -4,9 +4,10 @@ CRAZYNESS_LEVEL = 0
 
 CONTENT_DIR_PATH = 'content'
 WRITEN_TOPICS_FILE_PATH = 'writen_topics.csv'
+REPO_NAME = 'Hystoria'
 
 if os.environ.get("GITHUB_ACTIONS"):
-    WORKDIR = '/home/runner/work/AIHistorianBlogger/AIHistorianBlogger'
+    WORKDIR = f'/home/runner/work/{REPO_NAME}/{REPO_NANE}'
     CONTENT_DIR_PATH = f'{WORKDIR}/content'
     WRITEN_TOPICS_FILE_PATH = f'{WORKDIR}/writen_topics.csv'
 else:
